f6f2bfc5cf5f6724275c6078c2dfb4dd1568c68c
On2021 <dhkdrnfl4578@knou.ac.kr>
2022-07-26 23:02:01 +0900

    2022.07.26 퀵정렬/계수정렬/sorted 정렬라이브러리

---
+++ Ex04/ex6-7.py
@@ -0,0 +1,14 @@
+#-*- coding: utf-8 -*-
+'''
+Created on 2022. 7. 26.
+
+@author: dhkdr
+'''
+
+# 정렬 - sorted 정렬라이브러리
+# 예제 : sorted 소스코드 (p176)
+
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+result = sorted(array)
+print(result)
