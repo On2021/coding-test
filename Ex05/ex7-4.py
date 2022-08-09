d52e9d39dc28d486b1035424c73a3143ec558a76
On2021 <dhkdrnfl4578@knou.ac.kr>
2022-08-09 21:53:18 +0900

    2022.08.09 이진탐색 예제4

---
+++ Ex05/ex7-4.py
@@ -0,0 +1,15 @@
+#-*- coding: utf-8 -*-
+'''
+Created on 2022. 8. 9.
+
+@author: dhkdr
+'''
+
+# 이진탐색 예제4 (p196)
+
+import sys
+# 하나의 문자열 데이터 입력 받기
+input_data = sys.stdin.readline().rstrip()
+
+# 입력받은 문자열 그대로 출력
+print(input_data)
